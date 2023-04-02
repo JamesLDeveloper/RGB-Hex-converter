0215dd8f30c0b37e265ddc2d449d565a57ac2254
James L <jameslloyd2007@hotmail.co.uk>
2023-04-02 21:17:14 +0100

    try hex_val validity check

--- Script1.py
+++ Script1.py
@@ -40,9 +40,47 @@ def rgb_hex():
 def hex_rgb():
     hex_val = raw_input("Please enter a six digit hexadecimal color value to convert to an RGB: ")
     invalid_length_msg = "Sorry that input is not valid, please try again."
+
+    if hex_val[0] != 0 and hex_val[0] !=1 and hex_val[0] != 2 and hex_val[0] != 3 and hex_val[0] != 4 \
+    and hex_val[0] != 5 and hex_val[0] != 6 and hex_val[0] != 7 and hex_val[0] != 8 and hex_val[0] != 9 \
+    and hex_val[0].upper() != "A" and hex_val[0].upper() != "B" and hex_val[0].upper() != "C" and hex_val[0].upper() != "D" \
+    and hex_val[0].upper() != "E" and hex_val[0].upper() != "F" and \
+    hex_val[1] != 0 and hex_val[1] !=1 and hex_val[1] != 2 and hex_val[1] != 3 and hex_val[1] != 4 \
+    and hex_val[1] != 5 and hex_val[1] != 6 and hex_val[1] != 7 and hex_val[1] != 8 and hex_val[1] != 9 \
+    and hex_val[1].upper() != "A" and hex_val[1].upper() != "B" and hex_val[1].upper() != "C" and hex_val[1].upper() != "D" \
+    and hex_val[1].upper() != "E" and hex_val[1].upper() != "F" and \
+    hex_val[2] != 0 and hex_val[2] !=1 and hex_val[2] != 2 and hex_val[2] != 3 and hex_val[2] != 4 \
+    and hex_val[2] != 5 and hex_val[2] != 6 and hex_val[2] != 7 and hex_val[2] != 8 and hex_val[2] != 9 \
+    and hex_val[2].upper() != "A" and hex_val[2].upper() != "B" and hex_val[2].upper() != "C" and hex_val[2].upper() != "D" \
+    and hex_val[2].upper() != "E" and hex_val[2].upper() != "F" and \
+    hex_val[3] != 0 and hex_val[3] !=1 and hex_val[3] != 2 and hex_val[3] != 3 and hex_val[3] != 4 \
+    and hex_val[3] != 5 and hex_val[3] != 6 and hex_val[3] != 7 and hex_val[3] != 8 and hex_val[3] != 9 \
+    and hex_val[3].upper() != "A" and hex_val[3].upper() != "B" and hex_val[3].upper() != "C" and hex_val[3].upper() != "D" \
+    and hex_val[3].upper() != "E" and hex_val[3].upper() != "F" and \
+    hex_val[4] != 0 and hex_val[4] !=1 and hex_val[4] != 2 and hex_val[4] != 3 and hex_val[4] != 4 \
+    and hex_val[4] != 5 and hex_val[4] != 6 and hex_val[4] != 7 and hex_val[4] != 8 and hex_val[4] != 9 \
+    and hex_val[4].upper() != "A" and hex_val[4].upper() != "B" and hex_val[4].upper() != "C" and hex_val[4].upper() != "D" \
+    and hex_val[4].upper() != "E" and hex_val[4].upper() != "F" and \
+    hex_val[5] != 0 and hex_val[5] !=1 and hex_val[5] != 2 and hex_val[5] != 3 and hex_val[5] != 4 \
+    and hex_val[5] != 5 and hex_val[5] != 6 and hex_val[5] != 7 and hex_val[5] != 8 and hex_val[5] != 9 \
+    and hex_val[5].upper() != "A" and hex_val[5].upper() != "B" and hex_val[5].upper() != "C" and hex_val[5].upper() != "D" \
+    and hex_val[5].upper() != "E" and hex_val[5].upper() != "F" and \
+    hex_val[6] != 0 and hex_val[6] !=1 and hex_val[6] != 2 and hex_val[6] != 3 and hex_val[6] != 4 \
+    and hex_val[6] != 5 and hex_val[6] != 6 and hex_val[6] != 7 and hex_val[6] != 8 and hex_val[6] != 9 \
+    and hex_val[6].upper() != "A" and hex_val[6].upper() != "B" and hex_val[6].upper() != "C" and hex_val[6].upper() != "D" \
+    and hex_val[6].upper() != "E" and hex_val[6].upper() != "F" and \
+    hex_val[7] != 0 and hex_val[7] !=1 and hex_val[7] != 2 and hex_val[7] != 3 and hex_val[7] != 4 \
+    and hex_val[7] != 5 and hex_val[7] != 6 and hex_val[7] != 7 and hex_val[7] != 8 and hex_val[7] != 9 \
+    and hex_val[7].upper() != "A" and hex_val[7].upper() != "B" and hex_val[7].upper() != "C" and hex_val[7].upper() != "D" \
+    and hex_val[7].upper() != "E" and hex_val[7].upper() != "F":
+
+        print invalid_length_msg
+        return
+
     if len(hex_val) != 6:
         print invalid_length_msg
         return
+
     else:
         hex_val = int(hex_val, 16)
     two_hex_digits = 2 ** 8
